5af24d8e1044575f4356f38595ed0b9f346a6d1b
Ekimerton <ekim0252@gmail.com>
2024-12-09 17:50:18 -0500

    Updated navbar and globals.css

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,12 +1,24 @@
 import { Button } from "./ui/button";
 import Link from "next/link";
+import { Github, Twitter } from "lucide-react";
 
 export default function Navbar({}) {
   return (
     <nav className="bg-stone-100 dark:bg-stone-950 top-0 w-full z-10">
       <div className="max-w-4xl mx-auto pt-4 px-5 flex justify-center max-sm:justify-center items-center text-stone-700 dark:text-stone-300">
-        <div className="flex-1"></div>
-        <div className="flex items-center justify-center gap-2">
+        <div className="flex-1">
+          <Button variant="link" asChild size="icon">
+            <Link href={"https://github.com/Ekimerton/"} target="_blank">
+              <Github className="w-6 h-6 fill-stone-950 dark:fill-stone-50" />
+            </Link>
+          </Button>
+          <Button variant="link" asChild size="icon">
+            <Link href={"https://x.com/Ekimerton"} target="_blank">
+              <Twitter className="w-6 h-6 fill-stone-950 dark:fill-stone-50" />
+            </Link>
+          </Button>
+        </div>
+        <div className="flex items-center justify-center">
           <Button variant="link" asChild>
             <Link href={"/"} className="font-semibold">
               Home
@@ -19,11 +31,15 @@ export default function Navbar({}) {
           </Button>
         </div>
         <div className="flex-1 flex justify-end items-end">
-          {/*
           <Button variant="link" asChild>
-            <Link href={"/blog"}>Github</Link>
+            <Link
+              href={"https://www.10xportfolio.com/"}
+              target="_blank"
+              className="font-semibold text-yellow-500 dark:text-yellow-400"
+            >
+              Buy this <span className="max-sm:hidden"> &nbsp;portfolio</span>
+            </Link>
           </Button>
-          */}
         </div>
       </div>
     </nav>
