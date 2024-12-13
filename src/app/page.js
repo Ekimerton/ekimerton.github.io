741ba20890b4320619a8fa131d9f2badd03508a5
Ekimerton <ekim0252@gmail.com>
2024-12-13 10:47:21 -0500

    Moderized colors

--- src/app/page.js
+++ src/app/page.js
@@ -15,8 +15,8 @@ export default function Home() {
       <main className="min-h-screen w-full">
         {/*<Navbar />*/}
         <Hero />
-        <div className="flex items-center justify-center dark:bg-stone-950 bg-stone-100">
-          <article className="prose prose-stone dark:prose-invert dark:prose-p:text-stone-400 prose-p:text-stone-700 dark:prose-blockquote:text-stone-300 prose-blockquote:text-stone-800 max-w-4xl w-full px-5 py-8 prose-h2:font-semibold prose-h1:font-bold">
+        <div className="flex items-center justify-center dark:bg-slate-950 bg-slate-100">
+          <article className="prose prose-slate dark:prose-invert dark:prose-p:text-slate-400 prose-p:text-slate-700 dark:prose-blockquote:text-slate-300 prose-blockquote:text-slate-800 max-w-4xl w-full px-5 py-8 prose-h2:font-semibold prose-h1:font-bold">
             <AboutSection />
             {/*<ExperienceSection />*/}
             <ProjectsSection />
