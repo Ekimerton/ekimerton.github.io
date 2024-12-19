b04f29c0974ec2bfde6695fc916e6ce94e8c7d4b
Ekimerton <ekim0252@gmail.com>
2024-12-18 21:18:45 -0500

    Removed experience section

--- src/app/page.js
+++ src/app/page.js
@@ -16,7 +16,7 @@ export default function Home() {
         <div className="flex items-center justify-center dark:bg-zinc-950 bg-zinc-100">
           <article className="prose prose-zinc dark:prose-invert max-w-4xl w-full px-5 py-8 prose-h2:font-semibold prose-h1:font-bold">
             <AboutSection />
-            <ExperienceSection />
+            {/* <ExperienceSection /> */}
             <ProjectsSection />
             {/*<EducationSection />*/}
             <BlogSection />
