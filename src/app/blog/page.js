5358428c5ef40610ff48abc7972ffb25c6176857
Ekimerton <ekim0252@gmail.com>
2025-01-23 21:08:57 -0500

    Fixed page size

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -6,7 +6,7 @@ export default async function BlogHome() {
   const posts = await loadAllPosts();
 
   return (
-    <main className="min-h-screen w-full dark:bg-neutral-950 bg-neutral-100 flex justify-center">
+    <main className="h-full w-full dark:bg-neutral-950 bg-neutral-100 flex justify-center">
       <article className="prose prose-neutral dark:prose-invert max-w-3xl w-full px-4 py-4 mt-24 prose-h1:text-base prose-h2:text-base prose-h3:text-base">
         <div>
           <h3>Blog Posts</h3>
