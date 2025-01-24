873023cbd1114317a71581907edc3d6c6233d943
Ekimerton <ekim0252@gmail.com>
2025-01-24 15:27:35 -0500

    Changed colors back on blog

--- src/app/blog/[slug]/page.js
+++ src/app/blog/[slug]/page.js
@@ -35,7 +35,7 @@ export default async function Post({ params }) {
     <div className="">
       {/*<Hero />*/}
       <div className="flex justify-center dark:bg-stone-950 bg-stone-100">
-        <article className="prose prose-stone dark:prose-invert max-w-2xl w-full px-5 py-4 pt-12 min-h-screen prose-code:py-1 prose-code:px-2 prose-h2:font-semibold prose-h1:font-bold prose-p:text-stone-500 prose-p:dark:text-stone-400">
+        <article className="prose prose-stone dark:prose-invert max-w-2xl w-full px-5 py-4 pt-12 min-h-screen prose-code:py-1 prose-code:px-2 prose-h2:font-semibold prose-h1:font-bold">
           <Link
             href="/blog"
             className="text-sm no-underline text-stone-700 dark:text-stone-400 flex items-center mb-10 font-semibold"
