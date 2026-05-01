c06c1f03b605d7f91f149afed48be505fd60bc13
Ekim Karabey <ekim0252@gmail.com>
2026-04-30 22:52:56 -0700

    Small change to style

--- src/app/components/TextContainer.js
+++ src/app/components/TextContainer.js
@@ -6,9 +6,10 @@ export default function TextContainer({ children, className }) {
     <article
       className={twMerge(
         clsx(
-          `prose prose-stone dark:prose-invert max-w-3xl w-full p-8 antialiased
+          `prose prose-rose dark:prose-invert max-w-3xl w-full p-8 antialiased
            prose-h1:font-medium prose-h1:text-2xl
-           prose-h2:font-medium prose-h2:mt-6 prose-h2:mb-2 prose-h2:text-xl
+           prose-h2:font-medium prose-h2:text-xl
+           prose-h3:font-medium prose-h3:text-base
            prose-p:my-5
            prose-code:py-1 prose-code:px-2`,
           className
